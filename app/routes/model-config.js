61956a1e5d06f5f605e0d31132ae9b66f52e2187
fxghqc <konxiao@gmail.com>
2015-11-24 22:37:54 +0800

    recursive tree-view component

--- app/routes/model-config.js
+++ app/routes/model-config.js
@@ -3,5 +3,27 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   renderTemplate() {
     this.render('model-config');
+  },
+  model() {
+    return [
+      {
+        name: 'item', displayName: '实体', domain: 'data', type: 'persistent', category: 'item',
+        children: [{
+          name: 'resource', displayName: '资源', domain: 'data', type: 'persistent', category: 'item'
+        }, {
+          name: 'idItem', displayName: '代号类', domain: 'data', type: 'persistent', category: 'item',
+          children: [{
+            name: 'formItem', displayName: '表单类', domain: 'data', type: 'persistent', category: 'item'
+          }]
+        }, {
+          name: 'system', displayName: '系统类', domain: 'data', type: 'persistent', category: 'item'
+        }]
+      },
+      {name: 'relation', displayName: '关联', domain: 'data', type: 'dynamic', category: 'relation'},
+      {name: 'view', displayName: '视图', domain: 'data', type: 'dynamic', category: 'view'},
+      {name: 'module', displayName: '模块', domain: 'app', type: 'dynamic', category: 'module'},
+      {name: 'operation', displayName: '操作', domain: 'app', type: 'dynamic', category: 'operation'},
+      {name: 'event', displayName: '事件', domain: 'app', type: 'dynamic', category: 'event'}
+    ];
   }
 });
